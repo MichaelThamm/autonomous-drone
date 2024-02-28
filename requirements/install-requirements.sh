61e279873845da4f5323b88dab6d940b56a34064
Michael Thamm <84816828+MichaelThamm@users.noreply.github.com>
2024-02-27 21:23:17 -0500

    docs

--- requirements/install-requirements.sh
+++ requirements/install-requirements.sh
@@ -5,13 +5,22 @@ sudo apt update && sudo apt upgrade
 # Configure all packages
 sudo dpkg --configure -a
 
+# SSH
+sudo apt install ssh net-tools
+# Get IP address
+ifconfig
+
 # Enable SSH
 # https://linuxize.com/post/how-to-enable-ssh-on-ubuntu-20-04/
-sudo apt install openssh-server -Y
+sudo apt install openssh-server
 # Enable VNC
 # https://serverastra.com/docs/Tutorials/Setting-Up-and-Securing-SSH-on-Ubuntu-22.04%3A-A-Comprehensive-Guide
 
+# Git
+# NOTE: the rest of this code can be executed after git clone
+sudo apt install git
+
 # Virtual environment
 apt install virtualenv
-# Python
 
+# Python
