d75ef0a9e7af88542d7cce7d1a36f152591cfa14
Michael Thamm <mikethamm44@gmail.com>
2024-03-01 07:55:40 +0000

    Update requirements

--- requirements/install-requirements.sh
+++ requirements/install-requirements.sh
@@ -1,20 +1,31 @@
-# OS: Ubuntu 20.04.5 Server LTS
+# OS: Ubuntu 20.04.5 LTS
 # https://askubuntu.com/questions/1348560/is-ubuntu-20-04-desktop-for-raspberry-pi-4-no-longer-available
 
-# Update remotes
+# Update Indexes and Packages
 sudo apt update && sudo apt upgrade
+# Install Ubuntu Desktop
 sudo apt-get install ubuntu-desktop
-
-# SSH
-sudo apt install -y ssh
-
+# Install Other Packages
+sudo apt install -y curl ssh git virtualenv
 # Enable RDP in Sharing settings
 
 # Git
-# NOTE: the rest of this code can be executed after git clone
-sudo apt install -y git
+git config --global user.name "Michael Thamm"
+git config --global user.email "mikethamm44@gmail.com"
 
-# Virtual environment
-sudo apt install -y virtualenv
+# ROS
+## Enable Source Code in Software & Updates
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s  https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install -y ros-noetic-desktop-full
+## Automatically source ros setup on shell launch
+echo ". /opt/ros/noetic/setup.bash" >> ~/.bashrc
+. ~/.bashrc
+## Install rosinstall
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+## Initialize rosdep
+sudo rosdep init
+rosdep update
 
-# Python
+# DroneKit
