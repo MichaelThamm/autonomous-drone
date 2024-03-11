432b7840667461ed1fd95c6831784219045d3e26
Michael Thamm <84816828+MichaelThamm@users.noreply.github.com>
2024-03-11 12:39:06 -0400

    Docs Update

---
+++ requirements/install-oak.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://docs.luxonis.com/en/latest/pages/tutorials/first_steps/#first-steps-with-depthai
+# https://robofoundry.medium.com/oak-d-lite-camera-ros2-setup-1e74ed03350d
+# https://docs-beta.luxonis.com/software/ros/depthai-ros/intro/
