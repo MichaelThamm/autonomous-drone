35c94962755de1b3554568f7ae053fa70c5fa8eb
Michael Thamm <84816828+MichaelThamm@users.noreply.github.com>
2024-03-12 09:55:03 -0400

    Docs Update

--- requirements/install-oak.sh
+++ requirements/install-oak.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# I installed the RPi_64bit_OS.xz image
+# On Windows OS with PuTTY an X client is required: https://kb.iu.edu/d/bdnt
 # https://docs.luxonis.com/en/latest/pages/tutorials/first_steps/#first-steps-with-depthai
 # https://robofoundry.medium.com/oak-d-lite-camera-ros2-setup-1e74ed03350d
 # https://docs-beta.luxonis.com/software/ros/depthai-ros/intro/
