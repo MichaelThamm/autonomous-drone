075ed342dd17a8dec1c89c4acc71d37c4ae33eb3
Michael Thamm <84816828+MichaelThamm@users.noreply.github.com>
2024-03-15 13:16:38 +0000

    Docs Update

--- requirements/install-oak.sh
+++ requirements/install-oak.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-# I installed the RPi_64bit_OS.xz image
-# On Windows OS with PuTTY an X client is required: https://kb.iu.edu/d/bdnt
-# https://docs.luxonis.com/en/latest/pages/tutorials/first_steps/#first-steps-with-depthai
 # https://robofoundry.medium.com/oak-d-lite-camera-ros2-setup-1e74ed03350d
 # https://docs-beta.luxonis.com/software/ros/depthai-ros/intro/
+
+# Flash the RPi with the RPi_64bit_OS.xz image:
+# https://docs.luxonis.com/projects/hardware/en/latest/pages/guides/raspberrypi.html
+
+# For SSH from host to RPi
+ssh pi@luxonis.local -X
+# On Windows OS with PuTTY an X client is required
+# https://kb.iu.edu/d/bdnt
+
+# 1) Navigate to luxonis
+cd ~/Desktop/luxonis
+# 2) Activate environment
+source envDepthAI/bin/activate
+# 3) Run example to test
+python depthai-python/examples/MobileNet/rgb_mobilenet.py
+
+# Install DepthAi Viewer
+# Note: The depthai-viewer did not work for me
+# https://github.com/luxonis/depthai-viewer
+
+# Core Electronics Youtube Vid
+# https://core-electronics.com.au/guides/raspberry-pi/oak-d-lite-raspberry-pi/
+# Enable the Virtual Environment within the Tools Options in Thonny
+# https://nascentmaker.com/tutorials/2022/02/08/how-to-use-virtualenv-in-thonny.html
