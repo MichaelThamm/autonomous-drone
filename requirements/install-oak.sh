56cdea8754efec9c557295b5e59189601379e2fc
Michael Thamm <mike.thamm@canonical.com>
2025-01-19 19:51:18 -0500

    Update oak scripts

--- requirements/install-oak.sh
+++ requirements/install-oak.sh
@@ -8,15 +8,14 @@
 
 # For SSH from host to RPi
 ssh pi@luxonis.local -X
-# On Windows OS with PuTTY an X client is required
-# https://kb.iu.edu/d/bdnt
 
 # 1) Navigate to luxonis
 cd ~/Desktop/luxonis
 # 2) Activate environment
 source envDepthAI/bin/activate
-# 3) Run example to test
-python depthai-python/examples/MobileNet/rgb_mobilenet.py
+# 3) Run examples to test
+python3 depthai/depthai_demo.py
+python3 depthai-python/examples/MobileNet/rgb_mobilenet.py
 
 # Install DepthAi Viewer
 # Note: The depthai-viewer did not work for me
@@ -24,5 +23,3 @@ python depthai-python/examples/MobileNet/rgb_mobilenet.py
 
 # Core Electronics Youtube Vid
 # https://core-electronics.com.au/guides/raspberry-pi/oak-d-lite-raspberry-pi/
-# 1. In terminal (or thonny) set the interpreter to /home/pi/Desktop/luxonis/envDepthAI/bin/python3
-# 2. Run the ~/Desktop/luxonis/depthai/depthai_demo.py
