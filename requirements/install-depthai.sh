314f98e7460ba856080790d98785e1952d1aac82
Michael Thamm <mike.thamm@canonical.com>
2025-01-21 21:31:24 -0500

    justfiles

--- requirements/install-depthai.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/drone-project
-git clone https://github.com/luxonis/depthai-core
-cd depthai-core
-
-git submodule update --init --recursive
-cmake -S. -Bbuild
-cmake --build build
